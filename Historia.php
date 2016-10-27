31fc0e4f3808a4b0a3c4e2312241bc8133a1e2bb
Cristhian Velasquez <cristhianvg1991@gmail.com>
2016-10-27 10:00:06 -0500

    creacion del index al 50%
    1.Se termino el slider (Carrusel) de las reseñas de los expoBordados anteriores.
    2.Se termino el slider (Carrusel) de los diseñadores.
    3.se termino la parte de los botones de youtube,facebook, etc.
    4.se terminoi contenedor del dia/mes/año
    5.se termino la parte de los patrocinadores y de los derechos reservados.
    6.se creo el enlace desde el menu hacia la pagina de historia de expoBordados
    7.se termino la pagina historia.

---
+++ Historia.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/estiloPagHistoria.css">
+    </head>
+    <body>
+        <div class="container-fluid" id="contenedor1">
+            <p><img src="img/Logo.png"></p>
+        </div>
+        <div class="container-fluid" id="contenedor2">
+            <h1>Proyección</h1>
+            <p>Se busca afianzar y posicionar el evento<br>
+                a nivel nacional y proyectarlo a nivel<br>
+                internacional mediante las alianzas<br>
+                estratégicas con la Cámara de<br>
+                Comercio Colombo Americana en<br>
+                Miami, y el consulado de Colombia en<br>
+                New York, entre otros; para que<br>
+                Cartago sea reconocido mundialmente<br>
+                como Capital del Bordado y el Talento.</p>
+            <br>
+            <h1>Objetivo</h1>
+            <p>Bajo el modelo de un evento de City<br>
+                Marketing, vender la ciudad de<br>
+                Cartago, aprovechando su tradición,<br>
+                arquitectura, idiosincrasia, y su gran<br>
+                elemento diferenciador por el que es<br>
+                conocido a nivel nacional e internacional<br>
+                “El Bordado”, logrando así que el Norte<br>
+                del Valle sea visto como un polo de<br>
+                desarrollo en la moda del País; creando<br>
+                sentido de pertenencia y culturizando a<br>
+                la gente con esta propuesta.
+            </p>
+            <br>
+            <h1>Diseñadores</h1>
+            <p>Se convocaran diseñadores importantes<br>
+                de talla nacional, quienes presentaran<br>
+                en pasarela colecciones inéditas con<br>
+                técnicas de bordado y calado.</p>
+            <br>
+            <h1>Modelos</h1>
+            <p>Participaran las más significativas<br>
+                Agencias de Modelos de Bogotá y el<br>
+                Valle del Cauca; además algunas<br>
+                importantes Top Models del País lo cual<br>
+                dará una mayor proyección al evento.</p>
+        </div>
+        <div>
+            <a href="index.php">Regresar</a>
+        </div>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -9,99 +9,231 @@
 
     <?php require 'src/menuTop.php' ?>
 
-    <div class="contenedor">
-      <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+        <div class="contenedor">
+            <div id="carousel-example-generic2" class="carousel slide" data-ride="carousel">
 
-        <!-- Wrapper for slides -->
-        <div class="carousel-inner" role="listbox">
-          <div class="item active">
-            <img src="img/flor1.jpg" alt="...">
-            <div class="carousel-caption">
-              hola
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner" role="listbox">
+                    <div class="item active">
+                        <img src="img/flor1.jpg" alt="...">
+                        <div class="carousel-caption">
+                            <div class="TextOpaco">
+                                <p>ExpoBordado 2016</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img src="img/flor2.jpg" alt="...">
+                        <div class="carousel-caption">
+                            <div class="TextOpaco">
+                                <p>ExpoBordado 2015</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img src="img/flor3.jpg" alt="...">
+                        <div class="carousel-caption">
+                            <div class="TextOpaco">
+                                <p>ExpoBordado 2014</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img src="img/flor4.jpg" alt="...">
+                        <div class="carousel-caption">
+                            <div class="TextOpaco">
+                                <p>ExpoBordado 2013</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Controls -->
+                <a class="left carousel-control" href="#carousel-example-generic2" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#carousel-example-generic2" role="button" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
             </div>
-          </div>
-          <div class="item">
-            <img src="img/flor2.jpg" alt="...">
-            <div class="carousel-caption">
-              ...
-            </div>
-          </div>
-          <div class="item">
-            <img src="img/flor3.jpg" alt="...">
-            <div class="carousel-caption">
-              ...
-            </div>
-          </div>
-          <div class="item">
-            <img src="img/flor4.jpg" alt="...">
-            <div class="carousel-caption">
-              ...
-            </div>
-          </div>
         </div>
 
-        <!-- Controls -->
-        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-      </div>
-    </div>
-    
-    <div class="contenedor2">
-      <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+        <div class="contenedor2">
+            <div id="carousel-example-generic1" class="carousel slide" data-ride="carousel">
 
-        <!-- Wrapper for slides -->
-        <div class="carousel-inner" role="listbox">
-          <div class="item active">
-            <h1>ExpoBordado</h1>
-            <div class="div1" style="background-image: url('img/img1.jpg'); width: 300px; height: 300px;">
-              
+                <ol class="carousel-indicators">
+                    <li data-target="#carousel-example-generic1" data-slide-to="0" class="active"></li>
+                    <li data-target="#carousel-example-generic1" data-slide-to="1"></li>
+                    <li data-target="#carousel-example-generic1" data-slide-to="2"></li>
+                    <li data-target="#carousel-example-generic1" data-slide-to="3"></li>
+                    <li data-target="#carousel-example-generic1" data-slide-to="5"></li>
+                    <li data-target="#carousel-example-generic1" data-slide-to="6"></li>
+                    <br>
+                    <li data-target="#carousel-example-generic1" data-slide-to="7"></li>
+                    <li data-target="#carousel-example-generic1" data-slide-to="8"></li>
+                    <li data-target="#carousel-example-generic1" data-slide-to="9"></li>
+                    <li data-target="#carousel-example-generic1" data-slide-to="10"></li>
+                    <li data-target="#carousel-example-generic1" data-slide-to="11"></li>
+                    <li data-target="#carousel-example-generic1" data-slide-to="12"></li>
+                </ol>
+
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner" role="listbox">
+
+                    <div class="item active">
+                        <div class="div1">
+                            <h1>ExpoBordado 2016</h1>
+                        </div>
+                        <div class="div2">
+                            <p><img border="2" src="img/Patrocinador1.png" width="210" height="210" alt="..."></p>
+                        </div>
+                        <br>
+                        <div class="carousel-caption">
+                                <p>Lorem Ipsum ha sido el texto<br>
+                                de relleno estándar de las<br>
+                                Lorem Ipsum ha sido el texto<br>
+                                Lorem Ipsum ha sido el texto<br>
+                                de relleno estándar de las<br>
+                                Lorem Ipsum ha sido el texto<br>
+                                de relleno estándar de las
+                                </p>
+                        </div>
+                        <div>
+                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bs-example-modal-lg">Video 1</button>
+                        </div>
+                    </div>
+
+                    <div class="item">
+                        <h1 style="text-align: center;">ExpoBordado 2016</h1>
+                        <div class="div1" style="background-image: url('img/img2.jpg'); width: 300px; height: 300px;"></div>
+                        <div class="carousel-caption">
+                            Lorem Ipsum ha sido el texto<br>
+                            de relleno estándar de las<br>
+                            industrias desde el año 1500,
+                        </div>
+                        <!-- Large modal -->
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bs-example-modal-lg" id="btnVideo">Video</button>
+                    </div>
+
+                    <div class="item">
+                        <h1 style="text-align: center;">ExpoBordado 2016</h1>
+                        <div class="div1" style="background-image: url('img/img3.jpg'); width: 300px; height: 300px;"></div>
+                        <div class="carousel-caption">
+                            Lorem Ipsum ha sido el texto<br>
+                            de relleno estándar de las<br>
+                            industrias desde el año 1500,<br>
+                            cuando un impresor (N. del T.<br>
+                            persona que se dedica a la<br>
+                            imprenta) desconocido usó una<br>
+                            galería de textos y los mezcló<br>
+                            de tal manera que logró hacer<br>
+                            un libro de textos especimen.
+                        </div>
+                        <!-- Large modal -->
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bs-example-modal-lg" id="btnVideo">Video</button>
+                    </div>
+
+                    <div class="item">
+                        <h1 style="text-align: center;">ExpoBordado 2016</h1>
+                        <div class="div1" style="background-image: url('img/img4.jpg'); width: 300px; height: 300px;"></div>
+                        <div class="carousel-caption">
+                            Lorem Ipsum ha sido el texto<br>
+                            de relleno estándar de las<br>
+                            industrias desde el año 1500,<br>
+                            cuando un impresor (N. del T.<br>
+                            persona que se dedica a la<br>
+                            imprenta
+                        </div>
+                        <!-- Large modal -->
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bs-example-modal-lg" id="btnVideo">Video</button>
+                    </div>
+
+                </div>
+
+                <!-- Controls -->
+                <a class="left carousel-control" href="#carousel-example-generic1" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#carousel-example-generic1" role="button" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
             </div>
-            <div class="carousel-caption">cristhian
-            </div>
-          </div>
-          <div class="item">
-            <img src="img/img2.jpg" alt="...">
-            <div class="carousel-caption">
-              ...
-            </div>
-          </div>
-          <div class="item">
-            <img src="img/img3.jpg" alt="...">
-            <div class="carousel-caption">
-              ...
-            </div>
-          </div>
-          <div class="item">
-            <img src="img/img4.jpg" alt="...">
-            <div class="carousel-caption">
-              ...
-            </div>
-          </div>
         </div>
 
-        <!-- Controls -->
-        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-      </div>
-    </div>
+        <div class="contenedor3">
+            <div id="carousel-example-generic3" class="carousel slide" data-ride="carousel">
 
-    <?php require 'src/footer.php' ?>
-    <script>
-      $(document).ready(function () {
-        $('.carousel').carousel();
-      });
-    </script>
-  </body>
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner" role="listbox" id="carousel-inner3">
+                    <div class="item active">
+                        <a data-toggle="modal" data-target=".bs-example-modal-lg" ><img src="img/Diseñador1.jpg" alt="..."></a>
+                        <a data-toggle="modal" data-target=".bs-example-modal-lg" ><img src="img/Diseñador2.jpg" alt="..."></a>
+                        <a data-toggle="modal" data-target=".bs-example-modal-lg" ><img src="img/Diseñador3.png" alt="..."></a>
+                        <a data-toggle="modal" data-target=".bs-example-modal-lg" ><img src="img/Diseñador4.jpg" alt="..."></a>
+                        <div class="carousel-caption">
+                        </div>
+                    </div>
+                    
+                    <div class="item">
+                        <a data-toggle="modal" data-target=".bs-example-modal-lg" ><img src="img/Diseñador5.jpg" alt="..."></a>
+                        <a data-toggle="modal" data-target=".bs-example-modal-lg" ><img src="img/Diseñador6.jpg" alt="..."></a>
+                        <a data-toggle="modal" data-target=".bs-example-modal-lg" ><img src="img/Diseñador7.jpg" alt="..."></a>
+                        <a data-toggle="modal" data-target=".bs-example-modal-lg" ><img src="img/Diseñador8.jpg" alt="..."></a>
+                        <div class="carousel-caption">
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Controls -->
+                <a class="left carousel-control" href="#carousel-example-generic3" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#carousel-example-generic3" role="button" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+        
+        <div class="contenedor4">
+            <p><a data-toggle="modal" data-target=".bs-example-modal-lg"><img border="2" src="img/youtube.png" width="120" height="120" alt="..."></a></p>
+            <p><a data-toggle="modal" data-target=".bs-example-modal-lg"><img border="2" src="img/instagram.png" width="120" height="120" alt="..."></a></p>
+            <p><a data-toggle="modal" data-target=".bs-example-modal-lg"><img border="2" src="img/facebook.png" width="120" height="120" alt="..."></a></p>
+            <p><a data-toggle="modal" data-target=".bs-example-modal-lg"><img border="2" src="img/twitter.png" width="120" height="120" alt="..."></a></p>
+        </div>
+        
+        <div class="contenedor5">
+            <p><a data-toggle="modal" data-target=".bs-example-modal-lg"><img border="2" src="img/Logo.png" width="120" height="120" alt="..."></a></p>
+            <p>Dia/Mes/Año</p>
+        </div>
+        
+        <div class="contenedor6">
+            <p><a data-toggle="modal" data-target=".bs-example-modal-lg"><img border="2" src="img/Patrocinador1.png" width="210" height="210" alt="..."></a></p>
+            <p><a data-toggle="modal" data-target=".bs-example-modal-lg"><img border="2" src="img/Patrocinador1.png" width="210" height="210" alt="..."></a></p>
+        </div>
+        
+        <div class="contenedor7">
+            <p>© Todos los derechos recervados</p>
+        </div>
+
+        <div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                    ...
+                </div>
+            </div>
+        </div>
+
+        <?php require 'src/footer.php' ?>
+        <script>
+            $(document).ready(function () {
+                $('.carousel').carousel();
+            });
+        </script>
+    </body>
 </html>
